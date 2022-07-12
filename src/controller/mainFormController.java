64a055572274859d3318b3324b2e857d7b3091b6
Hiruni Piumika <piumikahiruni22@gmail.com>
2022-07-12 10:44:07 +0530

    add CrudUtil and db

--- src/controller/mainFormController.java
+++ src/controller/mainFormController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class mainFormController {
+    public TextField txtStId;
+    public TextField txtStName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtNic;
+    public Button btnAdd;
+    public Button btnUpdate;
+    public Button btnDelete;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/db/DbConnection.java
+++ src/db/DbConnection.java
@@ -1,4 +1,25 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+    private static DbConnection dbConnection = null;
+    private Connection connection;
+
+
+    private DbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ManageStudent ", "root", "1234");
+    }
+
+
+    public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return(dbConnection==null)? dbConnection= new DbConnection(): dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection; // DBConnection.getInstance().getConnection();
+    }
 }
